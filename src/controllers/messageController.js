2ae2a8013128ba95df3caa82e9312af767a282f2
ebenezermakinde <ebenezermakinde@yahoo.co.uk>
2019-03-07 17:56:22 +0100

    feat(sent): implement get all set emails
    * add method to controllers
    * add necessary route to router
    
    [Delivers #164351723]

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -7,5 +7,21 @@ class MessageController {
       data: messages,
     });
   }
+
+  static getSentEmail(req, res) {
+    const sent = messages.filter(message => message.status === 'sent');
+    if (!sent) {
+      return res.status(404).json({
+        status: 404,
+        error: 'No sent items',
+      });
+    }
+    return res.status(200).json({
+      status: 200,
+      data: sent,
+    });
+  }
 }
+
+
 export default MessageController;
--- src/routes/messageRoutes.js
+++ src/routes/messageRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import MessageController from '../controllers/messageController';
 
-const { getAllMessages } = MessageController;
+const { getAllMessages, getSentEmail } = MessageController;
 
 const messageRouter = express.Router();
 
 messageRouter.get('', getAllMessages);
+messageRouter.get('/sent', getSentEmail);
 
 export default messageRouter;
