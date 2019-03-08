9f8cb1ee6de4a866ede38b76422eb28be209fc0f
ebenezermakinde <ebenezermakinde@yahoo.co.uk>
2019-03-08 01:01:53 +0100

    feat(send email): implement send email
    * add send method to controller
    * add route to handle send
    * add validator to check user input
    
    [Delivers #164351726]

--- src/routes/messageRoutes.js
+++ src/routes/messageRoutes.js
@@ -1,11 +1,17 @@
 import express from 'express';
-import MessageController from '../controllers/messageController';
-
-const { getAllMessages, getSentEmail, getUnreadEmail } = MessageController;
+import {
+  getAllMessages, getSentEmail, getUnreadEmail, sendEmail,
+} from '../controllers/messageController';
+import MessageValidator from '../middleware/messageValidator';
 
 const messageRouter = express.Router();
 
+// Destructure email validator for the class.
+const { emailValidator } = MessageValidator;
+
+// Message routes
 messageRouter.get('', getAllMessages);
+messageRouter.post('', emailValidator, sendEmail);
 messageRouter.get('/sent', getSentEmail);
 messageRouter.get('/unread', getUnreadEmail);
 
